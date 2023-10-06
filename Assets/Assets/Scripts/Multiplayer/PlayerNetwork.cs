25e2b50bb6a134c651114aa8eada824fa4974521
Josep <josep.romera@enti.cat>
2023-10-06 17:45:31 +0200

    Continuacion, posiblemente acabe aqui la rama

--- Assets/Assets/Scripts/Multiplayer/PlayerNetwork.cs
+++ Assets/Assets/Scripts/Multiplayer/PlayerNetwork.cs
@@ -5,17 +5,44 @@ using UnityEngine;
 
 public class PlayerNetwork : NetworkBehaviour
 {
-    private NetworkVariable<int> randomNumber = new NetworkVariable<int>(1,NetworkVariableReadPermission.Everyone,NetworkVariableWritePermission.Owner);
+    private NetworkVariable<MyCustomData> randomNumber = new NetworkVariable<MyCustomData>(
+        new MyCustomData
+        {
+            _int = 56,
+            _bool = true,
+        }, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
     // Update is called once per frame
 
+    public struct MyCustomData : INetworkSerializable
+    {
+        public int _int;
+        public bool _bool;
+
+        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+        {
+            serializer.SerializeValue(ref _int);
+            serializer.SerializeValue(ref _bool);
+        }
+    }
+    public override void OnNetworkSpawn()
+    {
+        randomNumber.OnValueChanged += (MyCustomData previousValue, MyCustomData newValue) =>
+        {
+            Debug.Log(OwnerClientId + "; " + newValue._int + ";"+newValue._bool);
+        };
+    }
     void Update()
     {
-        Debug.Log(OwnerClientId + "; randomNumber: " + randomNumber.Value);
+        
         if (!IsOwner) return;
 
         if (Input.GetKeyDown(KeyCode.T))
         {
-            randomNumber.Value = Random.Range(0, 100);
+            randomNumber.Value = new MyCustomData
+            {
+                _int = 10,
+                _bool = false,
+            };
         }
         Vector3 moveDir = new Vector3(0, 0, 0);
         if (Input.GetKey(KeyCode.W)) moveDir.z = +1f;
