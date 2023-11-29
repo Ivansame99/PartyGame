1325861834e3bfd353049b299c3b083dea0bee37
Ivan Sales <ivan.sales@enti.cat>
2023-11-30 00:32:00 +0100

    Muerte y Respawn de jugador, falta hacer controlador de fin de partida

--- Assets/Scripts/PlayerHealthController.cs
+++ Assets/Scripts/PlayerHealthController.cs
@@ -21,6 +21,9 @@ public class PlayerHealthController : MonoBehaviour
     [SerializeField]
     private Transform staminaBar;
 
+    [SerializeField]
+    private Transform powerBar;
+
     private float timer;
 
     //Variables que iran donde se spawneen los pjs
@@ -31,8 +34,23 @@ public class PlayerHealthController : MonoBehaviour
     private Camera camera;
 
     private PlayerController playerController;
+    private PowerController powerController;
+    private CapsuleCollider playerCollider;
+    //private CapsuleCollider playerCollider;
 
     private Animator anim;
+
+    private float deathCD = 2;
+    private float deathTimer = 0;
+
+    private bool dead = false;
+    private bool deadAux = false;
+    private bool restart = false;
+
+    private float respawnTimer;
+
+    [SerializeField]
+    private float respawnCD;
 
     // Start is called before the first frame update
     void Start()
@@ -42,6 +60,8 @@ public class PlayerHealthController : MonoBehaviour
         SetupStaminaBar(healBarCanvas, camera);
         health = maxHealth;
         playerController = GetComponent<PlayerController>();
+        powerController = GetComponent<PowerController>();
+        playerCollider = GetComponent<CapsuleCollider>();
         anim = GetComponent<Animator>();
     }
 
@@ -53,6 +73,17 @@ public class PlayerHealthController : MonoBehaviour
         {
             timer -= Time.deltaTime;
         }
+
+        if (deathTimer >= 0)
+        {
+            deathTimer -= Time.deltaTime;
+        } else if (dead==true && !deadAux)
+        {
+            deadAux = true;
+            StartCoroutine(ScaleUpAndDown(this.transform, new Vector3(0f, 0f, 0f), 1f));
+        }
+
+        if (restart) Respawn();
     }
 
     public void ReceiveDamage(float damage)
@@ -70,11 +101,54 @@ public class PlayerHealthController : MonoBehaviour
     void Die()
     {
         anim.SetTrigger("Death");
-
+        deathTimer = deathCD;
+        dead = true;
+        respawnTimer = respawnCD;
+        DisablePlayer();
         /*float destroyDelay = Random.value;
         Destroy(this.gameObject, destroyDelay);
         Destroy(healthBar.gameObject, destroyDelay);*/
         //anim.SetBool("Death", false);
+    }
+
+    void DisablePlayer()
+    {
+        playerController.enabled = false;
+        powerController.enabled = false;
+        healthBar.gameObject.SetActive(false);
+        staminaBar.gameObject.SetActive(false);
+        powerBar.gameObject.SetActive(false);
+        
+        //playerCollider.enabled = false;
+    }
+
+    void EnablePlayer()
+    {
+        playerController.enabled = true;
+        playerController.dodge = false; //Por si estaba rodando cuando murio
+        powerController.enabled = true;
+        healthBar.gameObject.SetActive(true);
+        staminaBar.gameObject.SetActive(true);
+        powerBar.gameObject.SetActive(true);
+        dead = false;
+        deadAux = false;
+        restart = false;
+        health=maxHealth;
+        playerController.ResetStamina();
+        //anim.enabled = true;
+    }
+
+    void Respawn()
+    {
+        if (respawnTimer > 0)
+        {
+            respawnTimer -= Time.deltaTime;
+        }
+        else
+        {
+            this.transform.position = Vector3.zero;
+            EnablePlayer();
+        }
     }
 
     public void SetupHealthBar(Canvas canvas, Camera camera)
@@ -97,9 +171,26 @@ public class PlayerHealthController : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Wall")
+        if (other.gameObject.tag == "Wall" && !dead)
         {
             Die();
         }
+    }
+
+    IEnumerator ScaleUpAndDown(Transform transform, Vector3 upScale, float duration)
+    {
+        Vector3 initialScale = transform.localScale;
+
+        for (float time = 0; time < duration * 2; time += Time.deltaTime)
+        {
+            //float progress = Mathf.PingPong(time, duration) / duration;
+            transform.localScale = Vector3.Lerp(initialScale, upScale, time);
+            yield return null;
+        }
+        restart = true;
+        anim.SetTrigger("Respawn");
+        //Respawn();
+        //this.transform.position = 
+        //transform.localScale = initialScale;
     }
 }
