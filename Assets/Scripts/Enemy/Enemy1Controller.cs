af8a80920288b08f3bc5c539a734709287a5eb9f
Josep <josep.romera@enti.cat>
2023-11-29 19:19:07 +0100

    Avances enemigo

--- Assets/Scripts/Enemy/Enemy1Controller.cs
+++ Assets/Scripts/Enemy/Enemy1Controller.cs
@@ -5,73 +5,33 @@ using UnityEngine.AI;
 
 public class Enemy1Controller : MonoBehaviour
 {
-    [SerializeField] float health = 3;
-
-    [Header("Combat")]
-    [SerializeField] float attackCD = 3f;
-    [SerializeField] float attackRange = 1f;
-    [SerializeField] float aggroRange = 4f;
-
-    public List<Transform> players;
-    Animator animator;
-    NavMeshAgent agent;
-    float timePassed;
-    float newDestinationCD = 0.5f;
-    Transform player;
+    [SerializeField] private int HP = 9;
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
-        GameObject[] jugadoresArray = GameObject.FindGameObjectsWithTag("Player");
-        foreach (GameObject jugadorObj in jugadoresArray)
-        {
-            players.Add(jugadorObj.transform);
-        }
-        animator = GetComponent<Animator>();    
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        player = FindPlayer();
-        animator.SetFloat("speed",agent.velocity.magnitude / agent.speed);
-        
 
-        if(timePassed >= attackCD)
-        {
-            if(Vector3.Distance(player.transform.position,transform.position) <= attackRange)
-            {
-                animator.SetTrigger("attack");
-                timePassed = 0;
-            }
-        }
-        timePassed += Time.deltaTime;
-
-        if(newDestinationCD <= 0) //&& Vector3.Distance(player.transform.position,transform.position) < aggroRange)
-        {
-            newDestinationCD = 0.5f;
-            agent.SetDestination(player.transform.position);
-        }
-        newDestinationCD -= Time.deltaTime;
-        //ROTATIONS
-        // Calcula la dirección al jugador sin mirar hacia él
-        Vector3 direccionAlJugador = (player.position - transform.position).normalized;
-        direccionAlJugador.y = 0;
-        Quaternion targetRotation = Quaternion.LookRotation(direccionAlJugador);
-
-        // Aplica la rotación sin cambiar la inclinación
-        transform.rotation = Quaternion.Euler(0, targetRotation.eulerAngles.y, 0);
     }
-
-    public void TakeDamage(float damageAmount)
+    public void TakeDamage(int damageAmount)
     {
-        health -= damageAmount;
-        animator.SetTrigger("damage");
-        if(health < 0)
+        HP -= damageAmount;
+        if(HP < 0)
         {
-            Die();
+            animator.SetTrigger("die");
+        }
+        else
+        {
+            animator.SetTrigger("damage");
         }
     }
+
+    
 
     void Die()
     {
@@ -83,31 +43,8 @@ public class Enemy1Controller : MonoBehaviour
 
         if (other.CompareTag("SlashEffect"))
         {
-            TakeDamage(2);
+            TakeDamage(3);
         }
     }
-    private Transform FindPlayer()
-    {
-        Transform jugadorMasCercano = null;
-        float distanciaMinima = float.MaxValue;
 
-        foreach (Transform player in players)
-        {
-            float distancia = Vector3.Distance(transform.position, player.position);
-
-            if (distancia < distanciaMinima)
-            {
-                distanciaMinima = distancia;
-                jugadorMasCercano = player;
-            }
-        }
-
-        return jugadorMasCercano;
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, attackRange);
-    }
 }
