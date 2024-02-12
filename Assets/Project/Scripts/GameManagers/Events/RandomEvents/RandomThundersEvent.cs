3a89035556a0e844e555ad26c54d32b3f4a949fb
Pablo Perpiñan <pablo.perpinan@enti.cat>
2024-02-13 00:05:54 +0100

    soy bobo

--- Assets/Project/Scripts/GameManagers/Events/RandomEvents/RandomThundersEvent.cs
+++ Assets/Project/Scripts/GameManagers/Events/RandomEvents/RandomThundersEvent.cs
@@ -31,7 +31,7 @@ public class RandomThundersEvent : GameEvent
 	private float zPosMax = 19f;
 	private float zPosMin = -9f;
 	private float previewYPos = 0.9f;
-	private float attackYPos = 55f;
+	private float attackYPos = 76f;
 
 	private CoroutineManager coroutineManager;
 	private bool instanciateCoroutines;
