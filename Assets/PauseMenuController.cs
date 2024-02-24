629743ffefa46a9c7d35a9e1abb22b4f4d26fd73
Ivan Sales <ivan.sales@enti.cat>
2024-02-24 20:51:33 +0100

    Avances menu de pausa

--- Assets/PauseMenuController.cs
+++ Assets/PauseMenuController.cs
@@ -2,16 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static System.TimeZoneInfo;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class PauseMenuController : MonoBehaviour
 {
     private Animator anim;
     bool pause = false;
+	[SerializeField]
+	private GameObject defaultButton;
 
-    private void Awake()
+	[SerializeField]
+	private GameObject cursor;
+
+	[Header("Circle Transition")]
+	[SerializeField]
+	private Material transitionMaterial;
+	[SerializeField]
+	private float transitionTime = 2f;
+	[SerializeField]
+	private string propertyName = "_Progress";
+
+	private EventSystem eventSystem;
+
+	private GameObject lastButtonSelected;
+
+	private void Awake()
     {
         anim = GetComponent<Animator>();
-    }
+		eventSystem = EventSystem.current;
+	}
 
     void Start()
     {
@@ -29,10 +50,60 @@ public class PauseMenuController : MonoBehaviour
 				anim.SetBool("PauseAppear", false);
 			} else
             {
-                Time.timeScale = 0.0f;
+				eventSystem.SetSelectedGameObject(defaultButton);
+				cursor.transform.position = defaultButton.GetComponent<ButtonCursorPos>().cursorPos.position;
+				Time.timeScale = 0.0f;
                 pause = true;
 				anim.SetBool("PauseAppear", true);
 			}
 		}
+
+		if (pause)
+		{
+			CheckIfAnyButtonSelected();
+			if (eventSystem.currentSelectedGameObject != lastButtonSelected)
+			{
+				lastButtonSelected = eventSystem.currentSelectedGameObject;
+
+				cursor.transform.position = lastButtonSelected.GetComponent<ButtonCursorPos>().cursorPos.position;
+			}
+		}
+	}
+
+	void CheckIfAnyButtonSelected()
+	{
+		if (eventSystem.currentSelectedGameObject == null) eventSystem.SetSelectedGameObject(defaultButton);
+	}
+
+	public void UiContinueButton()
+    {
+		pause = false;
+		anim.SetBool("PauseAppear", false);
+		Time.timeScale = 1.0f;
+	}
+
+	public void UiSettingsButton()
+	{
+        Debug.Log("To do");
+	}
+
+	public void UiExitButton()
+	{
+		eventSystem.enabled = false;
+		StartCoroutine(CloseTranition());
+	}
+
+	private IEnumerator CloseTranition()
+	{
+		float currentTime = transitionTime;
+		while (currentTime > 0)
+		{
+			currentTime -= Time.unscaledDeltaTime;
+			transitionMaterial.SetFloat(propertyName, Mathf.Clamp01(currentTime / transitionTime));
+			yield return null;
+		}
+
+		Time.timeScale = 1.0f;
+		SceneManager.LoadScene("Menu");
 	}
 }
