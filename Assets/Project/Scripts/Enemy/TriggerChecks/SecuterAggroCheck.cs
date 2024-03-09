43be068acb37524bdf600fffa55aa9da75ac4264
Josep Romera Andreu <josep.romera@enti.cat>
2024-03-09 15:15:32 +0100

    Solucionado error de null Secutor

--- Assets/Project/Scripts/Enemy/TriggerChecks/SecuterAggroCheck.cs
+++ Assets/Project/Scripts/Enemy/TriggerChecks/SecuterAggroCheck.cs
@@ -38,7 +38,7 @@ public class SecuterAggroCheck : MonoBehaviour
             _enemy.SetAggroStatus(false);
         }
 
-        if (_enemy.playerPos2.position != null)
+        if (_enemy.playerPos2 != null)
         {
             distancePlayer2 = Vector3.Distance(_enemy.playerPos2.position, transform.position);
             if (distancePlayer2 < triggerDistanceClose)
