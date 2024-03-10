5d38e62cda0db03ee46321a42bed660cb6b57169
Ivan Sales <ivan.sales@enti.cat>
2024-03-10 19:55:46 +0100

    Un poco mejorado game over

--- Assets/Project/Scripts/Menu/GameOverController.cs
+++ Assets/Project/Scripts/Menu/GameOverController.cs
@@ -1,17 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class GameOverController : MonoBehaviour
 {
-    public void RestartButton()
+	[SerializeField]
+	private GameObject defaultButton;
+
+	[SerializeField]
+	private GameObject cursor;
+
+	[Header("Circle Transition")]
+	[SerializeField]
+	private Material transitionMaterial;
+	[SerializeField]
+	private float transitionTime = 3f;
+	[SerializeField]
+	private string propertyName = "_Progress";
+
+	private EventSystem eventSystem;
+
+	private GameObject lastButtonSelected;
+
+	private void Awake()
+	{
+		eventSystem = EventSystem.current;
+	}
+
+	public void Start()
+	{
+		transitionMaterial.SetFloat(propertyName, 1);
+	}
+
+	private void Update()
+	{
+		CheckIfAnyButtonSelected();
+
+		if (eventSystem.currentSelectedGameObject != lastButtonSelected)
+		{
+			lastButtonSelected = eventSystem.currentSelectedGameObject;
+
+			cursor.transform.position = lastButtonSelected.GetComponent<ButtonCursorPos>().cursorPos.position;
+		}
+	}
+
+	void CheckIfAnyButtonSelected()
+	{
+		if (eventSystem.currentSelectedGameObject == null) eventSystem.SetSelectedGameObject(defaultButton);
+	}
+
+	public void UiRestartButton()
     {
-        SceneManager.LoadScene("ArenaPowerLevel");
+		StartCoroutine(CloseTranition("ArenaPowerLevel"));
     }
 
-    public void ExitMenu()
+    public void UiExitMenu()
     {
-        SceneManager.LoadScene("Menu");
+		StartCoroutine(CloseTranition("Menu"));
     }
+
+	private IEnumerator CloseTranition(string levelName)
+	{
+		float currentTime = transitionTime;
+		while (currentTime > 0)
+		{
+			currentTime -= Time.deltaTime;
+			transitionMaterial.SetFloat(propertyName, Mathf.Clamp01(currentTime / transitionTime));
+			yield return null;
+		}
+
+		SceneManager.LoadScene(levelName);
+	}
 }
