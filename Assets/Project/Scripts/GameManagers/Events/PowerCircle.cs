462e85cdb194a36f2cc18ff82ab698d46a3fb72f
Ivan Sales <ivan.sales@enti.cat>
2024-04-07 21:39:40 +0200

    Seguido con la logica del prefab

--- Assets/Project/Scripts/GameManagers/Events/PowerCircle.cs
+++ Assets/Project/Scripts/GameManagers/Events/PowerCircle.cs
@@ -8,27 +8,41 @@ public class PowerCircle : MonoBehaviour
 	[SerializeField]
 	private TMP_Text text;
 
+	[SerializeField]
+	private GameObject particles;
+
+	[SerializeField]
+	private float reduceSpeed;
+
 	private int currentPlayersIn;
 	private int expectedPlayers;
 
 	private void Start()
 	{
-		Initialize(2);
+		//Initialize(2);
+		int numPlayers = GameManager.Instance.selectPlayerController.GetNumPlayers();
+		expectedPlayers = Random.Range(1, numPlayers+1);
+		Initialize(expectedPlayers);
 	}
 
 	private void Update()
 	{
 		if (currentPlayersIn == expectedPlayers)
 		{
-			Debug.Log("Empieza a hacerse pequeño");
+			transform.localScale -= new Vector3(reduceSpeed * Time.deltaTime, reduceSpeed * Time.deltaTime, 0f);
+
+			if (transform.localScale.x <= 0f && transform.localScale.y <= 0f)
+			{
+				Debug.Log("Repartir el poder");
+			}
 		}
 	}
 
 	public void Initialize(int players)
 	{
-		expectedPlayers = players;
+		//expectedPlayers = players;
 
-		//text.text = expectedPlayers.ToString() + "/" + GameManager.Instance.selectPlayerController.GetNumPlayers().ToString();
+		text.text = currentPlayersIn.ToString() + "/" + expectedPlayers.ToString();
 	}
 
 	private void OnTriggerEnter(Collider other)
@@ -36,6 +50,7 @@ public class PowerCircle : MonoBehaviour
 		if (other.CompareTag("Player"))
 		{
 			currentPlayersIn++;
+			text.text = currentPlayersIn.ToString() + "/" + expectedPlayers.ToString();
 		}
 	}
 
@@ -44,6 +59,7 @@ public class PowerCircle : MonoBehaviour
 		if (other.CompareTag("Player"))
 		{
 			currentPlayersIn--;
+			text.text = currentPlayersIn.ToString() + "/" + expectedPlayers.ToString();
 		}
 	}
 }
