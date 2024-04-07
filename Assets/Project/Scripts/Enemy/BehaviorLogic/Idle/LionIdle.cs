988bd57871ca2d53f6a18a6ca0b7d32eb7ba7940
Josep Romera Andreu <josep.romera@enti.cat>
2024-04-07 17:43:35 +0200

    gitflow-feature-stash: LionBossBasic

---
+++ Assets/Project/Scripts/Enemy/BehaviorLogic/Idle/LionIdle.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Lion Idle", menuName = "Enemy Logic/Boss/Lion/Status Logic/Idle")]
+public class LionIdle : EnemyIdleSOBase
+{
+    public override void DoAnimationTriggerEventLogic(Enemy.AnimationTriggerType triggerType)
+    {
+        base.DoAnimationTriggerEventLogic(triggerType);
+    }
+
+    public override void DoEnterLogic()
+    {
+        base.DoEnterLogic();
+        Debug.Log("funciona");
+    }
+
+    public override void DoExitLogic()
+    {
+        base.DoExitLogic();
+    }
+
+    public override void DoFrameUpdateLogic()
+    {
+        base.DoFrameUpdateLogic();
+    }
+
+    public override void DoPhysicsLogic()
+    {
+        base.DoPhysicsLogic();
+    }
+
+    public override void Init(GameObject gameObject, Enemy enemy)
+    {
+        base.Init(gameObject, enemy);
+    }
+
+    public override void ResetValues()
+    {
+        base.ResetValues();
+    }
+}
