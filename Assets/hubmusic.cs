fef6caa55763a58bb0f076102537c3e1182af957
attiliotonola <attilio.tonola@enti.cat>
2024-05-08 00:34:20 +0200

    hola

---
+++ Assets/hubmusic.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hubmusic : MonoBehaviour
+{
+    private static FMOD.Studio.EventInstance Music;
+    
+    void Start()
+    {
+        Music = FMODUnity.RuntimeManager.CreateInstance("event:/MUSIC/BS-TFG_FINAL_ARENA_NIEVE");
+        Music.start();
+        Music.release();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
