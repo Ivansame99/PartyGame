273fde0143d0b6870fd2ab41bd9e6c6c4545438d
Ivan Sales <ivan.sales@enti.cat>
2024-05-08 16:23:49 +0200

    Fondo creditos

--- Assets/Project/Scripts/Menu/CreditsController.cs
+++ Assets/Project/Scripts/Menu/CreditsController.cs
@@ -9,7 +9,7 @@ public class CreditsController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		if (Gamepad.current != null && Gamepad.current.buttonEast.isPressed)
+		if ((Gamepad.current != null && Gamepad.current.buttonEast.isPressed) || (Keyboard.current != null && Keyboard.current.escapeKey.wasPressedThisFrame))
 		{
 			SceneManager.LoadScene("Menu");
 		}
