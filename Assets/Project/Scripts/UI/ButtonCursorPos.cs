157829f898341665a480a09631553db65769fd05
Ivan Sales <ivan.sales@enti.cat>
2024-05-08 18:03:49 +0200

    Mas cambios

--- Assets/Project/Scripts/UI/ButtonCursorPos.cs
+++ Assets/Project/Scripts/UI/ButtonCursorPos.cs
@@ -5,4 +5,17 @@ using UnityEngine;
 public class ButtonCursorPos : MonoBehaviour
 {
     public Transform cursorPos;
+
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void SelectButton()
+    {
+		animator.ResetTrigger("Select");
+		animator.SetTrigger("Select");
+    }
 }
