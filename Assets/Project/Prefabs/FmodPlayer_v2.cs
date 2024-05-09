c47f75bf765581d7dbbede528fd6868b0ec67861
Ivan Sales <ivan.sales@enti.cat>
2024-05-10 00:57:09 +0200

    Pasos funcionando aparentemente

--- Assets/Project/Prefabs/FmodPlayer_v2.cs
+++ Assets/Project/Prefabs/FmodPlayer_v2.cs
@@ -3,43 +3,65 @@ using System.Collections.Generic;
 using System;
 using UnityEngine;
 
-public class FmodPlayer_v2 : MonoBehaviour {
-    private float distance = 0.1f;
-    private float Material;
+public class FmodPlayer_v2 : MonoBehaviour
+{
+	private float distance = 0.1f;
+	private float Material;
+	private float raycastDistance = 0.5f;
 
-    void PlayMeleeEvent(string path)
-    {
-        FMODUnity.RuntimeManager.PlayOneShot(path, transform.position);
-    }
+	[SerializeField]
+	private Transform raycastPoint;
 
-    void FixedUpdate()
-    {
-        MaterialCheck();
-        Debug.DrawRay(transform.position, Vector2.down * distance, Color.blue);
-    }
+	[SerializeField]
+	private LayerMask groundLayer;
 
-    void MaterialCheck()
-    {
-        RaycastHit2D hit;
-        
-        hit = Physics2D.Raycast(transform.position, Vector2.down, distance, 1 << 31);
+	void PlayMeleeEvent(string path)
+	{
+		FMODUnity.RuntimeManager.PlayOneShot(path, transform.position);
+	}
 
-        if (hit.collider)
-        {
-            if (hit.collider.tag == "Material: Sand")
-                Material = 1f;
-            else if (hit.collider.tag == "Material: Stone")
-                Material = 2f;
-            else
-                Material = 1f;
-        }
-    }
+	void FixedUpdate()
+	{
+		MaterialCheck();
+		Debug.DrawRay(transform.position, Vector2.down * distance, Color.blue);
+	}
 
-    void PlayFootstepsEvent(string path)
-    {
-        FMOD.Studio.EventInstance Footsteps = FMODUnity.RuntimeManager.CreateInstance(path);
-        Footsteps.setParameterByName("Material", Material);
-        Footsteps.start();
-        Footsteps.release();
-    }
+	void MaterialCheck()
+	{
+		if (Physics.Raycast(raycastPoint.position, Vector3.down, out RaycastHit hit, raycastDistance))
+		{
+			if (hit.collider)
+			{
+				if (hit.collider.tag == "Material: Sand")
+				{
+					Material = 1f;
+				}
+				else if (hit.collider.tag == "Material: Stone")
+				{
+					Material = 2f;
+				}
+				else
+					Material = 1f;
+			}
+		}
+
+		//Debug.Log(hit.collider);
+		//      if (hit.collider)
+		//      {
+		//          if (hit.collider.tag == "Material: Sand")
+		//              Material = 1f;
+		//          else if (hit.collider.tag == "Material: Stone")
+		//              Material = 2f;
+		//          else
+		//              Material = 1f;
+		//      }
+	}
+
+	void PlayFootstepsEvent(string path)
+	{
+		FMOD.Studio.EventInstance Footsteps = FMODUnity.RuntimeManager.CreateInstance(path);
+		Footsteps.setParameterByName("Material", Material);
+		Footsteps.start();
+		Footsteps.release();
+	}
 }
