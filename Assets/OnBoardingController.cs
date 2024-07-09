8c8abe6b578ca64c5f679bd726a6e4d25810fcc7
Ivan Sales <ivan.sales@enti.cat>
2024-07-09 23:53:58 +0200

    gitflow-feature-stash: OnBoardingvFinal

---
+++ Assets/OnBoardingController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnBoardingController : MonoBehaviour
+{
+	private Animator anim;
+	private bool onBoarding = false;
+
+	[SerializeField]
+	private GameObject[] practiceDolls;
+	// Start is called before the first frame update
+	void Start()
+	{
+		anim = this.GetComponent<Animator>();
+
+		if (PlayerPrefs.GetInt("onBoarding", 0) == 0)
+		{
+			onBoarding = false;
+		}
+		else
+		{
+			onBoarding = true;
+			anim.SetBool("Onboarding", true);
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (!onBoarding)
+		{
+			for(int i = 0; i < practiceDolls.Length; i++)
+			{
+				if (practiceDolls[i] != null) return;
+			}
+
+			onBoarding=true;
+			anim.SetBool("Onboarding", true);
+		}
+	}
+}
